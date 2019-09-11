a39f097c4899c900f274f426cf7d4add2c425159
temcadooAN <54277256+temcadooAN@users.noreply.github.com>
2019-09-11 04:47:36 -0500

    First Commit, Hello World!

---
+++ Hello-World/Hello-World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which is better, \"Hackers\" or \"The Matrix\"?");
+            Console.ReadLine();
+        }
+    }
+}
